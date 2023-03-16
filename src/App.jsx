fff8913dfb18737be575d44ca9bc91b34bb73316
tepavcevic <djordje.tepa@gmail.com>
2023-03-16 16:26:29 +0100

    File cleanup Cleaned up unnecessary files, initialized prettier

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import TaskInput from './components/task-input/Index';
+
+function App() {
+  return (
+    <div className="App">
+      <TaskInput />
+    </div>
+  );
+}
+
+export default App;
